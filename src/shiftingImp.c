c53aa5eb926e75677407f0bb861622eeb0500c2d
Shashwath Kumar <shashwath.basics@gmail.com>
2016-03-11 13:15:42 -0500

    bit shift functions ready

--- src/shiftingImp.c
+++ src/shiftingImp.c
@@ -12,6 +12,12 @@ int getBit(int num, int i){
 int clearMSBsTilli(int num, int i){
     return num & ((1<<i-1)-1);
 }
+int clearLSBsExi(int num, int i){
+    return num & (~((1<<i-1)-1));
+}
+int updateBit(int num, int i, int bitVal){
+    return (num & ~(1 << i-1)) | (bitVal << i-1);
+}
 
 int main(void) {
 	// your code goes here
@@ -31,13 +37,20 @@ int main(void) {
     printf("(setBit) num: %d\n", num);
     printf("num at 3    : %d\n", myBit);
     
-    num = clearMSBsTilli(num, 3);
-    myBit = getBit(num, 3);
+    num = clearMSBsTilli(num, 4);
+    myBit = getBit(num, 4);
     printf("(clear MSBs till i) num: %d\n", num);
+    printf("num at 4               : %d\n", myBit);
+    
+    num = clearLSBsExi(num, 3);
+    myBit = getBit(num, 3);
+    printf("(clear LSBs till i(excluded)) num: %d\n", num);
     printf("num at 3               : %d\n", myBit);
+    
+    num = updateBit(num, 4, 1);
+    myBit = getBit(num, 4);
+    printf("(clear LSBs till i(excluded)) num: %d\n", num);
+    printf("num at 4               : %d\n", myBit);
     
 	return 0;
 }
-
-
-    
